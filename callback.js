ec144aa3af01af48bbeca088fb9f7ac5107dcf4d
skshabeera <82590734+skshabeera@users.noreply.github.com>
2022-03-17 00:15:28 +0530

    Add files via upload

---
+++ callback.js
@@ -0,0 +1,15 @@
+let add = function(num1,num2){
+    return num1+num2;
+ }
+  
+ let multiply= function(num1,num2){
+    return num1*num2;
+ }
+  
+ let calc= function (num1,num2,callback){
+    console.log(callback(num1,num2));
+ }
+  
+ calc(2,3,add);
+ calc(2,3,multiply);
+ 
